0c348ef013b344d955a9ad69e2fcce87c2682690
PratikshaPate <pratiksha.pate@cumminscollege.in>
2014-07-31 10:51:24 +0530

    Update cal.cpp
    call function.

--- cal.cpp
+++ cal.cpp
@@ -3,8 +3,15 @@ void main()
 {
   cout<<"Welcome to github";
   cout<<"done by sujata and Pratiksha";
+  cout<<area(21)<<endl;
+  cout<<cube(23)<<endl;
 }
 int area(int a)
 {
   return(a*a);
+}
+
+double cube(doule b)
+{
+  return(b*b*b);
 }
