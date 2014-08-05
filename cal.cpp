829097542fb62fa3f92c4eed0a687997297dc4b0
priyanka jagtap <priyankajagtap2494@gmail.com>
2014-08-05 14:27:34 +0530

    edited
    edited

--- cal.cpp
+++ cal.cpp
@@ -2,13 +2,13 @@
 void main()
 {
   cout<<"Welcome to github";
-  cout<<"done by sujata and Pratiksha";
+  cout<<"done by sayali and madhuri";
   cout<<area(21)<<endl;
   cout<<cube(23)<<endl;
 }
 int area(int a)
 {
-  return(a*a);
+  return(a);
 }
 
 double cube(doule b)
